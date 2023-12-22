4739427ee9be5fc839a2fe21fd1ee3d5d638e02b
Valentine Silvansky <v.silvansky@gmail.com>
2023-12-22 12:49:18 +0300

    More options

--- README.md
+++ README.md
@@ -13,5 +13,15 @@ secret = secretwithoutquotes
 
 # Usage
 
+# Basic
+
 - Write your prompts to `input.txt` line by line
 - Run `python kanbagen.py`
+- All generated images are in `output/`
+
+## Available options
+
+- `-n` to add prompt number to filename
+- `-p` to add prompt to filename
+- `-o <dir>` to set output directory (defaults to `output`)
+- `-s <suffix>` to add custom suffix to all prompts, for example `-s "in surrealistic style"`
--- kanbagen.py
+++ kanbagen.py
@@ -3,6 +3,8 @@ import time
 import requests
 import base64
 import configparser
+import argparse
+import os
 
 class Text2ImageAPI:
 
@@ -55,25 +57,51 @@ if __name__ == '__main__':
     api_key = config['API']['key']
     api_secret = config['API']['secret']
 
-    print(f'Starting with API key: {api_key}, secret: <hidden>')
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-s", "--suffix", type=str, default="", help="Common prompt suffix")
+    argParser.add_argument("-o", "--output", type=str, default="output", help="Output directory")
+    argParser.add_argument("-n", "--nprefix", action='store_true', help="Add numeric prefix to images")
+    argParser.add_argument("-p", "--pprefix", action='store_true', help="Add prompt prefix to images")
+    args = argParser.parse_args()
+
+    print(f'Starting with arguments: {args}')
+
+    print(f'API key: {api_key}, secret: <hidden>')
+
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
 
     api = Text2ImageAPI('https://api-key.fusionbrain.ai/', api_key, api_secret)
     model_id = api.get_model()
     print(f'Using model: {model_id}')
 
-    # Using readlines()
     file = open('input.txt', 'r')
     prompts = file.readlines()
 
+    num = 1
+
     for prompt in prompts:
-        print(f'Generating with prompt: {prompt}')
-        uuid = api.generate(prompt, model_id)
+        prompt_with_suffix = prompt + " " + args.suffix
+        print(f'Generating with prompt: {prompt_with_suffix}')
+        uuid = api.generate(prompt_with_suffix, model_id)
         images = api.check_generation(uuid)
         img_data = bytes(images[0], "utf-8")
-        filename = f"image_{uuid}.jpg" 
-        with open(filename, "wb") as fh:
+        filename_prefix = "image"
+
+        if args.nprefix:
+            filename_prefix = f"{num:03d}"
+
+        if args.pprefix:
+            clean_string = filter(lambda x: x.isalnum() or x.isspace(), prompt)
+            filename_prefix = filename_prefix + "_" + "".join(clean_string).replace(" ", "_")
+        
+        filename = f"{filename_prefix}_{uuid}.jpg"
+
+        file_path = os.path.join(args.output, filename)  
+
+        with open(file_path, "wb") as fh:
             fh.write(base64.decodebytes(img_data))
-        print(f'Saved result as {filename}')
+        print(f'Saved result as {file_path}')
 
     print(f'Generation complete!')
 
