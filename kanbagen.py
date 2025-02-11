efe0281cfba60066b53c5dd496b4eba195ef7ef9
Valentine Silvansky <v.silvansky@gmail.com>
2025-02-11 10:18:15 +0300

    Better error handling

--- kanbagen.py
+++ kanbagen.py
@@ -6,6 +6,10 @@ import configparser
 import argparse
 import os
 
+class Text2ImageAPIError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
 class Text2ImageAPI:
 
     def __init__(self, url, api_key, secret_key):
@@ -19,6 +23,9 @@ class Text2ImageAPI:
         response = requests.get(self.URL + 'key/api/v1/text2image/availability?model_id=' + str(model), headers=self.AUTH_HEADERS)
         data = response.json()
         print(f'🔍 Availability: {data}')
+        status = data['status']
+        if status != 'ACTIVE':
+            raise Text2ImageAPIError(f"Model not available: {status}")
 
     def get_model(self):
         response = requests.get(self.URL + 'key/api/v1/models', headers=self.AUTH_HEADERS)
@@ -56,37 +63,38 @@ class Text2ImageAPI:
                 httpStatus = response.status_code
 
                 if httpStatus != 200:
-                    raise Exception(f"Bad HTTP status code: {httpStatus}, data: {data}")
+                    raise Text2ImageAPIError(f"Bad HTTP status code: {httpStatus}, data: {data}")
 
                 status = data['status']
 
                 if status == 'DONE':
                     if data['censored'] == True:
-                        raise Exception(f"Generation censored: {data['errorDescription']}")
-
-                    print(f"\nStatus: {status}, censored: {data['censored']}, error: {data['errorDescription']}")
+                        raise Text2ImageAPIError(f"Generation censored")
 
                     images = data['images']
 
                     # check images is array and contains at least 1 element
                     if not isinstance(images, list) or len(images) < 1:
-                        raise Exception(f"Unexpected response from server")
+                        raise Text2ImageAPIError(f"Unexpected response from server")
 
                     img_data = bytes(images[0], "utf-8")
                     return img_data
                 elif status == 'FAIL':
-                    raise Exception(f"Generation failed: {data['errorDescription']}")
+                    raise Text2ImageAPIError(f"Generation failed: {data}")
 
                 last_data = data
+            except Text2ImageAPIError as e:
+                print(f"\n❌ API error: {e}")
+                raise
             except Exception as e:
                 print(f"\n❌ Network error: {e}")
 
             attempt += 1
 
-            print(f"\r⏳ Waiting for {delay} seconds for generation to complete, attempt {attempt}/{attempts}, last data: {last_data}", end='', flush=True)
+            print(f"\r⏳ Waiting for {delay} seconds for generation to complete, attempt {attempt}/{attempts}", end='', flush=True)
             time.sleep(delay)
 
-        raise Exception(f"Failed! Last server response: {last_data}")
+        raise Text2ImageAPIError(f"Failed! Last server response: {last_data}")
 
 
 if __name__ == '__main__':
